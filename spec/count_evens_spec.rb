7c360bafd91bdb5eadc7fca9a4267a1ab5b3d91b
laurenjrichie <laurenjrichie@gmail.com>
2014-12-27 15:05:21 -0500

    set up array challenge files; wrote say_hello test

---
+++ spec/count_evens_spec.rb
@@ -0,0 +1,2 @@
+require 'spec_helper'
+require 'count_evens'
---
+++ spec/say_hello_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'say_hello'
+
+describe Array do
+  it 'has a method say_hello' do
+    array = Array.new
+
+    expect(array.say_hello).to eq(
+      "HEY THERE, I'M AN ARRAY! SOMEONE FUCKED WITH MY CODE :("
+    )
+  end
+
+end
