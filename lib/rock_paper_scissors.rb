80144a663bcfd2fa376caf8c826b03b9bdaadc75
laurenjrichie <laurenjrichie@gmail.com>
2014-12-30 12:20:43 -0500

    rock paper scissors tests passing

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,38 +1,40 @@
 class RockPaperScissors
   def prompt
-    puts "Welcome to Rock, Paper, Scissors. What's your choice?"
+    "Welcome to Rock, Paper, Scissors. What's your choice?"
   end
 
   def user_chooses
-    @user_choice = gets.chomp
+    gets.chomp
   end
 
   def computer_chooses
-    @computer_choice = ["rock", "paper", "scissors"].sample
+    choice = ["rock", "paper", "scissors"].sample
+    puts "Computer chooses #{choice}."
+    choice
   end
 
-  def evaluate
+  def evaluate(user_choice, computer_choice)
     winner = ''
-    if @user_choice == "rock"
-      if @computer_choice == "rock"
+    if user_choice == "rock"
+      if computer_choice == "rock"
         winner = "It's a tie!"
-      elsif @computer_choice == "paper"
+      elsif computer_choice == "paper"
         winner = "Paper wins!"
       else
         winner = "Rock wins!"
       end
-    elsif @user_choice == "paper"
-      if @computer_choice == "rock"
+    elsif user_choice == "paper"
+      if computer_choice == "rock"
         winner = "Paper wins!"
-      elsif @computer_choice == "paper"
+      elsif computer_choice == "paper"
         winner = "It's a tie!"
       else
         winner = "Scissors wins!"
       end
     else
-      if @computer_choice == "rock"
+      if computer_choice == "rock"
         winner = "Rock wins!"
-      elsif @computer_choice == "paper"
+      elsif computer_choice == "paper"
         winner = "Scissors wins!"
       else
         winner = "It's a tie!"
@@ -42,11 +44,10 @@ class RockPaperScissors
   end
 
   def run
-    prompt
-    user_chooses
-    computer_chooses
-    puts "Computer chooses #{@computer_choice}."
-    puts evaluate
+    puts prompt
+    user_choice = user_chooses
+    computer_choice = computer_chooses
+    puts evaluate(user_choice, computer_choice)
   end
 
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -6,8 +6,7 @@ describe RockPaperScissors do
     @game = RockPaperScissors.new
   end
 
-  xit 'prompts the user for a choice' do
-    # not working
+  it 'prompts the user for a choice' do
     expect(@game.prompt).to include("What's your choice?")
   end
 
@@ -16,31 +15,31 @@ describe RockPaperScissors do
   end
 
   it 'scissors beats paper' do
-    @user_choice = "paper"
-    @computer_choice = "scissors"
-    # why is this failing?
-    expect(@game.evaluate).to eq("Scissors wins!")
+    user_choice = "paper"
+    computer_choice = "scissors"
+
+    expect(@game.evaluate(user_choice, computer_choice)).to eq("Scissors wins!")
   end
 
   it 'paper beats rock' do
-    @computer_choice = "paper"
-    @user_choice = "rock"
+    computer_choice = "paper"
+    user_choice = "rock"
 
-    expect(@game.evaluate).to eq("Paper wins!")
+    expect(@game.evaluate(user_choice, computer_choice)).to eq("Paper wins!")
   end
 
   it 'rock beats scissors' do
-    @computer_choice = "rock"
-    @user_choice = "scissors"
+    computer_choice = "rock"
+    user_choice = "scissors"
 
-    expect(@game.evaluate).to eq("Rock wins!")
+    expect(@game.evaluate(user_choice, computer_choice)).to eq("Rock wins!")
   end
 
   it 'its a tie' do
-    @computer_choice = "rock"
-    @user_choice = "rock"
+    computer_choice = "rock"
+    user_choice = "rock"
 
-    expect(@game.evaluate).to eq("It's a tie!")
+    expect(@game.evaluate(user_choice, computer_choice)).to eq("It's a tie!")
   end
 
 end
