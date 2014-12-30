9885ea740a990ac3b024fd94dc7293684fdbbc67
laurenjrichie <laurenjrichie@gmail.com>
2014-12-30 10:41:25 -0500

    temp convertor tests not passing either

--- spec/temp_convertor_spec.rb
+++ spec/temp_convertor_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
 require 'temp_convertor'
 
-describe TempConvertor
-  xit 'converts from C to F' do
+describe TempConvertor do
+  it 'converts from C to F' do
     convertor = TempConvertor.new
+    celsius = 25 # does this override what's in the method?
 
+    expect(convertor.celsius_to_fahrenheit).to eq(77.0)
   end
 
   xit 'converts from F to C' do
     convertor = TempConvertor.new
+
+    expect(convertor.fahrenheit_to_celsius).to eq()
   end
 
 end
